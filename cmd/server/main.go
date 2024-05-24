53f1ebdb253233893810618201da4474211a56a9
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-05-25 00:20:21 +0200

    (feat) Adds signal to close connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-    amqp "github.com/rabbitmq/amqp091-go"
-    "fmt"
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
 	fmt.Println("Starting Peril server...")
-    connStr := "amqp://guest:guest@localhost:5672/"
-    _, err :=  amqp.Dial(connStr)
-    if err != nil {
-        panic("jaaj")
-    }
-	fmt.Println("Connected")
+	connStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		panic("jaaj")
+	}
+	fmt.Println("Rabbit has been caught...")
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	defer conn.Close()
 }
