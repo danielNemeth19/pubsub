7dc5ccda36a7ad2f99dd47fa8c5b1ce7c4011dcb
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-05-26 00:10:52 +0200

    (feat) Sends first message to rabbitmq

--- Makefile
+++ Makefile
@@ -2,6 +2,10 @@
 start-rabbit:
 	podman run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
 
+.PHONY: tail-rabbit
+tail-rabbit:
+	podman run --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
+
 .PHONY: stop-rabbit
 stop-rabbit:
 	podman stop rabbitmq
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"pubsub/internal/pubsub"
+	"pubsub/internal/routing"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -12,12 +14,20 @@ func main() {
 	fmt.Println("Starting Peril server...")
 	connStr := "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(connStr)
+	defer conn.Close()
 	if err != nil {
-		panic("jaaj")
+		panic("Error establishing connection")
 	}
 	fmt.Println("Rabbit has been caught...")
+	myC, err := conn.Channel()
+	if err != nil {
+		panic("Rabbit channel failed to open")
+	}
+	err = pubsub.PublishJSON(myC, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		panic("Publising failed")
+	}
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	<-signalChan
-	defer conn.Close()
 }
