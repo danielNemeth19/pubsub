4f71b659790d9a495ce859aff7a73e251027e55e
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-06-01 12:35:42 +0200

    (feat) Passing code for transient exercise

--- internal/pubsub/interact.go
+++ internal/pubsub/interact.go
@@ -3,6 +3,8 @@ package pubsub
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -30,15 +32,40 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, simpleQueueType int)(*amqp.Channel, amqp.Queue, error) {
     chn, err := conn.Channel() 
     if err != nil {
-        panic("Channel cannot be created")
+        return nil, amqp.Queue{}, fmt.Errorf("Channel creation failed: %w", err)
     }
-    queue, err := chn.QueueDeclare("", simpleQueueType == Durable, simpleQueueType == Transient, simpleQueueType == Transient, false, nil )
+
+    fmt.Printf("Creating queue with name: %s, durable: %v, autoDelete: %v, exclusive: %v\n",
+        queueName, simpleQueueType == Durable, simpleQueueType == Transient, simpleQueueType == Transient)
+
+    queue, err := chn.QueueDeclare(queueName, simpleQueueType == Durable, simpleQueueType == Transient, simpleQueueType == Transient, false, nil )
+
     if err != nil {
-        panic("Queue declaration failed")
+        return nil, amqp.Queue{}, fmt.Errorf("Queue declaration failed: %w", err)
     }
-    err = chn.QueueBind("", key, exchange, false, nil)
+    fmt.Printf("Binding queue %s to exchange %s with key %s\n", queueName, exchange, key)
+    err = chn.QueueBind(queueName, key, exchange, false, nil)
     if err != nil {
-        panic("Bind did not work")
+        fmt.Println(err)
+        return nil, amqp.Queue{}, fmt.Errorf("Queue binding failed: %w", err)
     }
-    return chn, queue, err
+
+    time.Sleep(60 * time.Second)
+
+    fmt.Println("Queue bound successfully")
+    defer func() {
+        fmt.Println("Closing channel and connetion")
+        err := chn.Close()
+        if err != nil {
+            fmt.Println("Closing channel failed: ", err)
+
+        }
+        err = conn.Close()
+        if err != nil {
+            fmt.Println("Closing connection failed: ", err)
+
+        }
+    }()
+    return chn, queue, nil
 }
+
