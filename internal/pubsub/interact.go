61b7dbccfd920704ec5a0f643391224a9693905f
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-07-04 23:27:06 +0200

    (feat) Adds a method for gob serialization

--- internal/pubsub/interact.go
+++ internal/pubsub/interact.go
@@ -1,7 +1,9 @@
 package pubsub
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -53,6 +55,23 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		key,
 		false, false,
 		amqp.Publishing{ContentType: "application/json", Body: packet},
+	)
+	return err
+}
+func PublishGob[T any](ch *amqp.Channel, exchange, key string, val T) error {
+    var packet bytes.Buffer
+    encoder := gob.NewEncoder(&packet)
+    err := encoder.Encode(val)
+
+	if err != nil {
+		panic("Marshalling failed")
+	}
+	err = ch.PublishWithContext(
+		context.Background(),
+		exchange,
+		key,
+		false, false,
+		amqp.Publishing{ContentType: "application/gob", Body: packet.Bytes()},
 	)
 	return err
 }
